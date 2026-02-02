46065af4d2be257ab7d258f128dcb5b246f54e74
Hadi-rajput <hadi88689@gmail.com>
2026-02-02 19:42:04 +0500

    everthing is perfect but tommorrow i've to add description in the card

--- app.js
+++ app.js
@@ -3,17 +3,40 @@ const path = require('path');
 const pageController = require('./controllers/pageController');
 const bodyparser = require("body-parser");
 const connectdb = require('./config/db');
+const session = require('express-session');
 
 const app = express();
 
 // Connect to Database
 connectdb();
 
+
 // View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
+
+// Session Middleware
+app.use(session({
+    secret: 'my_linkkey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// function for auth name is auth
+
+function isAuth(req, res, next) {
+    if (req.session.userId) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
@@ -21,14 +44,26 @@ app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', pageController.homePage);
-app.get('/add-link', pageController.addLinkPage);
-app.get('/about', pageController.aboutPage);
-app.get('/signup',pageController.signupPage);
-app.get('/login',pageController.loginPage);
-app.post("/signup",pageController.postsignup);
+app.get('/', isAuth, pageController.homePage);
+app.get('/add-link', isAuth, pageController.addLinkPage);
+app.get('/about', isAuth, pageController.aboutPage);
+app.get('/signup', pageController.signupPage);
+app.get('/login', pageController.loginPage);
+app.post("/signup", pageController.postsignup);
+app.post("/login", pageController.postlogin);
+app.post("/add-link",isAuth, pageController.postaddlink);
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('error while logging out = > ' + err);
+        };
+        res.clearCookie("connect.sid");
+        res.redirect('/login');
+    });
+});
 
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log('server is running at http://localhost:3000');
-    
+
 })
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,14 @@
 const User = require("../models/userModel.js");
-const bcrypt = require('bcrypt')
-
-
-exports.homePage = (req, res) => {
-    res.render('index', { title: 'Home - LinkSaver' });
+const Link = require("../models/linkModel.js");
+const bcrypt = require('bcrypt');
+exports.homePage = async (req, res) => {
+    try {
+        const links = await Link.find({ userId: req.session.userId }).sort({ createdAt: -1 });
+        res.render('index', { title: 'Home - LinkSaver', name: req.session.userFullname, links });
+    } catch (error) {
+        console.error('Error loading home page links:', error);
+        res.status(500).send('Server error');
+    }
 };
 
 exports.addLinkPage = (req, res) => {
@@ -14,41 +19,84 @@ exports.aboutPage = (req, res) => {
     res.render('about', { title: 'About - LinkSaver' });
 };
 
-exports.signupPage = (req,res)=>{
-    res.render('signup',{title:'Signup - LinkSaver'});
+exports.signupPage = (req, res) => {
+    res.render('signup', { title: 'Signup - LinkSaver' });
 };
-exports.loginPage = (req,res)=>{
-    res.render('login',{title:'Login - LinkSaver'})
+exports.loginPage = (req, res) => {
+    res.render('login', { title: 'Login - LinkSaver' })
 }
 
-exports.postsignup = async(req,res)=>{
-    try{
-        let {fullname,email,password,confirmpassword} = req.body;
+exports.postsignup = async (req, res) => {
+    try {
+        let { fullname, email, password, confirmpassword } = req.body;
         console.log(req.body);
-        
-          email = email.toLowerCase();
 
-         const existinguser = await User.findOne({ email });
+        email = email.toLowerCase();
 
-if (existinguser) {
-    // User already exists
-    return res.status(400).send('User already exists');
-} else if (confirmpassword.trim() !== password.trim()) {
-    // Passwords do not match
-    return res.status(400).send("Passwords don't match");
-}
-          const hashpassword = await bcrypt.hash(password,10);
-          const newuser = new User({
+        const existinguser = await User.findOne({ email });
+
+        if (existinguser) {
+            // User already exists
+            return res.status(400).send('User already exists');
+        } else if (confirmpassword.trim() !== password.trim()) {
+            // Passwords do not match
+            return res.status(400).send("Passwords don't match");
+        }
+        const hashpassword = await bcrypt.hash(password, 10);
+        const newuser = new User({
             fullname,
             email,
-            password:hashpassword
-          });
+            password: hashpassword
+        });
 
-          await newuser.save();
+        await newuser.save();
 
-          res.redirect('/login');
-    }catch(error){
-        console.log('error while saving user and the error is this = > '+error);
-        
+        res.redirect('/login');
+    } catch (error) {
+        console.log('error while saving user and the error is this = > ' + error);
+
     }
 };
+exports.postlogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send('No user found with this email');
+        };
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send('Incorrect password');
+        };
+
+        //set up session
+
+        req.session.userId = user.id;
+        req.session.userFullname = user.fullname;
+
+        res.redirect('/');
+    } catch (error) {
+        console.log('error while logging in and the error is this = > ' + error);
+    }
+};
+
+exports.postaddlink = async (req, res) => {
+    const { title, url, description } = req.body;
+    try {
+        if (!title || !url) {
+            return res.status(400).send('Title and URL are required');
+        };
+        const newLink = new Link({
+            title,
+            url,
+            description,
+            userId: req.session.userId
+        });
+        await newLink.save();
+        res.redirect('/');
+    }
+    catch (error) {
+    console.log('error while adding link and the error is this = > ' + error);
+}
+
+}
